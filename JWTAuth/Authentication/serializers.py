88a6e741519156ca6ea38db9628e16baa1f8beaf
Indira kasichhwa <kasichhwaindira@gmail.com>
2024-10-28 09:35:31 +0545

    added changes

--- JWTAuth/Authentication/serializers.py
+++ JWTAuth/Authentication/serializers.py
@@ -20,6 +20,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         return user
     
-    class LoginSerializer(serializers.ModelSerializer):
-        username= serializers.CharField(required=True)
-        password=serializers.CharField(required=True , write_only=True)
+class LoginSerializer(serializers.ModelSerializer):
+    username= serializers.CharField(required=True)
+    password=serializers.CharField(required=True , write_only=True)
